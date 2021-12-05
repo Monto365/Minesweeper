3a5e2c621416f67f6a5c690a6d932d9a59dc1ff5
Joan <monto365@gmail.com>
2021-12-05 12:56:46 +0100

    Starting

--- Minesweeper_Test.py
+++ Minesweeper_Test.py
@@ -1,2 +1,12 @@
+import unittest
 import Minesweeper
 
+class Minesweeper(unittest.TestCase):
+    def test_dimensions(self):
+        #Arrange
+        level=Level()
+
+        #Act
+        set_level_dimensions(8, 8)
+
+        #Assertk
