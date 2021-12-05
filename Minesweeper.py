04dd26153742935167f336cbf5835e82a5a3ab85
Joan <monto365@gmail.com>
2021-12-05 14:22:49 +0100

    Test dimensions Test number_of_mines

--- Minesweeper.py
+++ Minesweeper.py
@@ -8,15 +8,11 @@ class Game:
 class Level:
     def __init__(self):
         self.dimensions = Dimensions
-        self.table = 0
+        self.number_of_mines = 0
 
     def set_level_dimensions(self, rows, columns):
         self.dimensions.rows = rows
         self.dimensions.columns = columns
-
-    # def set_level_dimensions(self, dimensions):
-    #     self.dimensions.set_number_of_columns(dimensions.columns)
-    #     self.dimensions.set_number_of_rows(dimensions.rows)
 
     def set_number_of_rows(self, number_of_rows):
         self.dimensions.rows = number_of_rows
@@ -30,10 +26,16 @@ class Level:
     def get_number_of_columns(self):
         return self.dimensions.columns
 
+    def calculate_number_of_mines(self):
+        total_places = self.get_number_of_rows() * self.get_number_of_columns()
+        self.number_of_mines = (total_places / 64) * 10
+
+    def get_number_of_mines(self):
+        self.calculate_number_of_mines()
+        return self.number_of_mines
+
 
 class Dimensions:
     def __init__(self):
         self.rows = 0
         self.columns = 0
-
-
--- Minesweeper_Test.py
+++ Minesweeper_Test.py
@@ -7,13 +7,25 @@ class Minesweeper(unittest.TestCase):
         # Arrange
         level = Level()
 
+        # Act
+        level.set_level_dimensions(8, 8)
+        rows = level.get_number_of_rows()
+        columns = level.get_number_of_columns()
+
+        # Assert
+        self.assertEqual(rows, 8)
+        self.assertEqual(columns, 8)
+
+    def test_number_of_mines(self):
+        # Arrange
+        level = Level()
 
         # Act
         level.set_level_dimensions(8, 8)
+        number_of_mines = level.get_number_of_mines()
 
         # Assert
-        self.assertEqual(level.get_number_of_rows(), 8)
-        self.assertEqual(level.get_number_of_rows(), 8)
+        self.assertEqual(number_of_mines, 10)
 
 
 if __name__ == "__main__":
