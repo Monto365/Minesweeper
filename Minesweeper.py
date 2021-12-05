d8caf052ea107430fe53c48541c63f32a84010fd
Joan <monto365@gmail.com>
2021-12-05 14:04:55 +0100

    Starting

--- Minesweeper.py
+++ Minesweeper.py
@@ -1,8 +1,39 @@
 import unittest
 
-class Minesweeper:
+
+class Game:
     pass
 
-if __name__ == '__main__':
-    print_hi('PyCharm')
+
+class Level:
+    def __init__(self):
+        self.dimensions = Dimensions
+        self.table = 0
+
+    def set_level_dimensions(self, rows, columns):
+        self.dimensions.rows = rows
+        self.dimensions.columns = columns
+
+    # def set_level_dimensions(self, dimensions):
+    #     self.dimensions.set_number_of_columns(dimensions.columns)
+    #     self.dimensions.set_number_of_rows(dimensions.rows)
+
+    def set_number_of_rows(self, number_of_rows):
+        self.dimensions.rows = number_of_rows
+
+    def set_number_of_columns(self, number_of_columns):
+        self.dimensions.columns = number_of_columns
+
+    def get_number_of_rows(self):
+        return self.dimensions.rows
+
+    def get_number_of_columns(self):
+        return self.dimensions.columns
+
+
+class Dimensions:
+    def __init__(self):
+        self.rows = 0
+        self.columns = 0
+
 
--- Minesweeper_Test.py
+++ Minesweeper_Test.py
@@ -1,12 +1,20 @@
 import unittest
-import Minesweeper
+from Minesweeper import *
+
 
 class Minesweeper(unittest.TestCase):
     def test_dimensions(self):
-        #Arrange
-        level=Level()
+        # Arrange
+        level = Level()
 
-        #Act
-        set_level_dimensions(8, 8)
 
-        #Assertk
+        # Act
+        level.set_level_dimensions(8, 8)
+
+        # Assert
+        self.assertEqual(level.get_number_of_rows(), 8)
+        self.assertEqual(level.get_number_of_rows(), 8)
+
+
+if __name__ == "__main__":
+    unittest.main()
