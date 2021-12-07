e71df4dbc529af38017dbd562394b95e35383e5b
Joan <joanGuilloRoman@gmail.com>
2021-12-07 19:12:12 +0100

    Table created

--- Minesweeper.py
+++ Minesweeper.py
@@ -9,10 +9,11 @@ class Level:
     def __init__(self):
         self.dimensions = Dimensions
         self.number_of_mines = 0
+        self.table = [[0] * self.get_number_of_rows()] * self.get_number_of_columns()
 
     def set_level_dimensions(self, rows, columns):
-        self.dimensions.rows = rows
-        self.dimensions.columns = columns
+        self.set_number_of_rows(rows)
+        self.set_number_of_columns(columns)
 
     def set_number_of_rows(self, number_of_rows):
         self.dimensions.rows = number_of_rows
@@ -35,7 +36,23 @@ class Level:
         return self.number_of_mines
 
 
+    def print_table(self):
+        table=''
+        for i in range(7):
+            for j in range(7):
+                table += str(self.table[i][j])
+            table += '\n'
+        return table
+
+    def get_table(self):
+        return self.table
+
+    def set_value_in_position_table(self, value, row, column, table):
+        pass
+
+    # def set_table(self,rows,columns):
+    #     tabletemp=[[0]*rows]*columns
+
 class Dimensions:
-    def __init__(self):
-        self.rows = 0
-        self.columns = 0
+    rows = 0
+    columns = 0
--- Minesweeper_Test.py
+++ Minesweeper_Test.py
@@ -1,6 +1,9 @@
 import unittest
 from Minesweeper import *
 
+# Show full diff in unittest
+unittest.util._MAX_LENGTH = 2000
+
 
 class Minesweeper(unittest.TestCase):
     def test_dimensions(self):
@@ -26,6 +29,18 @@ class Minesweeper(unittest.TestCase):
 
         # Assert
         self.assertEqual(number_of_mines, 10)
+
+    def test_create_table(self):
+        # Arrange
+        level = Level()
+
+        # Act
+        level.set_level_dimensions(8, 8)
+        table = level.table
+
+        # Assert
+
+        self.assertEqual(table, [[0] * level.get_number_of_rows()] * level.get_number_of_columns())
 
 
 if __name__ == "__main__":
